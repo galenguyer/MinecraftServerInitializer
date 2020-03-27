f78db5a10e9847a3d736b43b790f3e3fa81d081e
Galen Guyer <git@galenguyer.com>
2020-03-26 22:18:10 -0400

    Get latest version from the API

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 import sys
+import json
+from urllib.request import urlopen
 
 def get_is_root() -> bool:
     return os.getuid() == 0;
@@ -16,9 +18,14 @@ def check_root() -> None:
         else:
             sys.exit('Please run the script as root to continue')
 
+def get_latest_version() -> str:
+    json_data = urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json').read().decode('utf-8')
+    versions = json.loads(json_data)
+    return versions['latest']['release']
+
 def main() -> None:
-    pass
+    print(get_latest_version())
 
 if __name__ == '__main__':
-    check_root()
+    #check_root()
     main()
