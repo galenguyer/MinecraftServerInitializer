9f4f8504384da6c1067d0a978cf55ce14efa96fd
Galen Guyer <git@galenguyer.com>
2020-04-19 21:54:17 -0700

    Add cleanup messages and memory calc

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from urllib.request import urlopen, urlretrieve
 MC_VERSIONS = json.loads(urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json').read().decode('utf-8'))
 USER_VERSION = 'latest'
 FILE_PATH = os.getcwd()
-
+MEM_SIZE = min(((os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES'))/(1024.**3)), 4)
 
 def reporthook(count, block_size, total_size):
     global start_time
@@ -65,9 +65,25 @@ def download_server(url: str):
         FILE_PATH += '/'
     if os.path.exists(FILE_PATH) or os.access(os.path.dirname(FILE_PATH), os.W_OK):
         urlretrieve(url, f"{FILE_PATH}minecraft-server-{USER_VERSION}.jar", reporthook)
+        time.sleep(0.5)
         print(f"\nDownloaded minecraft-server-{USER_VERSION}.jar to {FILE_PATH}")
     else:
         sys.exit(f"'{FILE_PATH}' is not a valid or writeable path")
+
+
+
+def finish():
+    global MEM_SIZE
+    server_name = FILE_PATH.strip('/').split('/')[-1]
+    print("\nDone!")
+    print(f"If you opted to create a systemd service, start the server by running 'systemctl start {server_name}' as root")
+    print(f"Otherwise, run it with 'java -jar -Xms{MEM_SIZE}G -Xmx{MEM_SIZE}G minecraft-server-{USER_VERSION}.jar nogui'. The -Xm options refer to \
+minimum and maximum memory allocated to the JVM. Only edit these if you experience performance issues and you know what you're doing.")
+    exit(0)
+
+
+def create_systemd_file():
+    print("Haha not yet")
 
 
 def main() -> None:
@@ -79,6 +95,10 @@ def main() -> None:
         USER_VERSION = get_latest_version()
     download_server(get_vanilla_url(USER_VERSION))
     
+    create_systemd = input('Do you want a systemd unit to be created? [Yn]: ')
+    if create_systemd.lower() != 'n':
+        create_systemd_file()
+    finish()
 
 if __name__ == '__main__':
     #check_root()
