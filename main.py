b3be66e12a13cf7b896282d5acadf7ac344c7b8e
Galen Guyer <git@galenguyer.com>
2020-04-19 20:52:55 -0700

    Double lines between methods, thanks pycharm

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import sys
 import json
 from urllib.request import urlopen
 
+
 MC_VERSIONS = json.loads(urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json').read().decode('utf-8'))
 USER_VERSION = 'latest'
 
+
 def get_is_root() -> bool:
     return os.getuid() == 0;
+
 
 def check_root() -> None:
     if get_is_root():
@@ -21,8 +24,10 @@ def check_root() -> None:
         else:
             sys.exit('Please run the script as root to continue')
 
+
 def get_latest_version() -> str:
     return MC_VERSIONS['latest']['release']
+
 
 def get_vanilla_url(selected: str):
     for version in MC_VERSIONS['versions']:
@@ -31,6 +36,7 @@ def get_vanilla_url(selected: str):
             return version_json['downloads']['server']['url']
     sys.exit(f'Invalid version {selected}. Exiting')
 
+
 def main() -> None:
     uver = input(f'Input the version of Minecraft to use. The default is the lateset release ({get_latest_version()}): ')
     if uver:
@@ -38,6 +44,7 @@ def main() -> None:
     else:
         USER_VERSION = get_latest_version()
     print(get_vanilla_url(USER_VERSION))
+
 
 if __name__ == '__main__':
     #check_root()
